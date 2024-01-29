3532c97972f827ac8f49c79b3e2e2f592559f4c5
Tonny Tzeng <tonny.tzeng@gmail.com>
2024-01-29 22:42:43 +0800

    counter: porting to NETStart! board.

--- counter/counter.c
+++ counter/counter.c
@@ -3,7 +3,10 @@
 #include <asm/arch/hardware.h>
 #include <exports.h>
 
-#ifdef CONFIG_PLATFORM_EVALUATOR7T
+static void setup(void);
+static void show_digit(int digit);
+
+#if CONFIG_IS_ENABLED(PLATFORM_EVALUATOR7T)
 #define DIGIT0      0x17c00
 #define DIGIT1      0x01800
 #define DIGIT2      0x0ec00
@@ -17,19 +20,52 @@
 
 static const unsigned DigitMap[] = { DIGIT0, DIGIT1, DIGIT2, DIGIT3, DIGIT4, DIGIT5, DIGIT6, DIGIT7, DIGIT8, DIGIT9 };
 
+static void inline setup()
+{
+    clrbits(32, REG_IOPMODE, (1 << 8));     // Set I/O[8] (i.e. INT0) in input mode
+    setbits(32, REG_IOPMODE, (0x7f << 10)); // Set I/O[16:10] (i.e. 7-Segment LED) in output mode
+    out_32(REG_IOPCON, (in_32(REG_IOPCON) & ~0x1f) | 0x1d);
+}
+
+static void inline show_digit(int digit)
+{
+    clrbits(32, REG_IOPDATA, DigitMap[8]);
+    setbits(32, REG_IOPDATA, DigitMap[digit % 10]);
+}
+#elif CONFIG_IS_ENABLED(PLATFORM_NETSTART)
+#define DIGIT0      0x21
+#define DIGIT1      0xaf
+#define DIGIT2      0x32
+#define DIGIT3      0x26
+#define DIGIT4      0xac
+#define DIGIT5      0x64
+#define DIGIT6      0x60
+#define DIGIT7      0x2f
+#define DIGIT8      0x20
+#define DIGIT9      0x24
+
+static const uchar DigitMap[] = { DIGIT0, DIGIT1, DIGIT2, DIGIT3, DIGIT4, DIGIT5, DIGIT6, DIGIT7, DIGIT8, DIGIT9 };
+
+static void inline setup()
+{
+}
+
+static void inline show_digit(int digit)
+{
+    out_8(IO_BANK(1), DigitMap[digit % 10]);
+}
+#endif
+
 int main (int argc, char * const argv[])
 {
     register unsigned count = 0;
     app_startup(argv);
-    clrbits(32, REG_IOPMODE, (1 << 8));     // Set I/O[8] (i.e. INT0) in input mode
-    setbits(32, REG_IOPMODE, (0x7f << 10)); // Set I/O[16:10] (i.e. 7-Segment LED) in output mode
-    out_32(REG_IOPCON, (in_32(REG_IOPCON) & ~0x1f) | 0x1d);
 
+    setup();
     printf ("Press any key to exit ... ");
     while (!tstc()) {
-        clrbits(32, REG_IOPDATA, DigitMap[8]);
         if (++count > 9) count = 0;
-        setbits(32, REG_IOPDATA, DigitMap[count]);
+        show_digit(count);
         mdelay(1000);
     }
     (void) getc();
@@ -37,4 +73,3 @@ int main (int argc, char * const argv[])
 
     return (0);
 }
-#endif // CONFIG_PLATFORM_EVALUATOR7T
